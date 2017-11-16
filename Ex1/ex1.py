8f12743cfaf182cb9cbf6e99548c90e54df5a09a
Adar <adarlavi@gmail.com>
2017-11-16 16:01:09 +0200

    progress in 2, skelton of basic functions

--- Ex1/ex1.py
+++ Ex1/ex1.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 
 # Task 1: Spatial sampling
@@ -12,19 +13,29 @@ def getSampledImageAtResolution(dim, pixelSize, x):
 def optimalQuantizationImage(img, k):
     Q = np.random.randint(0, 256, k)
     Q = np.sort(Q)
-
+    epsilon = 0.003
     bounds = np.zeros(k + 1, int) #there's a chance it should be float
     for i in range(0, k + 1):
         bounds[i] = (Q[i + 1] - Q[i]) / 2
 
-    
+    while (calcError(k, Q,  ))
 
     return 0
 
-def calcError(k, *centroids, **probs):
-    for i in range(0,len(centroids)):
-        for j in range(0, len(centroids) + 1):
+#gets the image, returns array with # of appearances of each pixel [0-255]
+def countApperances(img):
+    numericalVal = cv2.imread(img)
+    appearances = np.zeros(256, int)
 
+    for x in np.nditer(numericalVal):
+        appearances[x] += 1
+
+    return appearances
+
+def calcError(k, centroids, appearances):
+    for i in range(0,len(centroids)):
+
+    return 0
 
 
 
