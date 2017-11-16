84c2fa33262aea87e288d337cc072b32f8d075c9
Adar <adarlavi@gmail.com>
2017-11-16 14:48:55 +0200

    base of part 2

--- Ex1/ex1.py
+++ Ex1/ex1.py
@@ -10,7 +10,22 @@ def getSampledImageAtResolution(dim, pixelSize, x):
 
 # Task 2: Quantization
 def optimalQuantizationImage(img, k):
+    Q = np.random.randint(0, 256, k)
+    Q = np.sort(Q)
+
+    bounds = np.zeros(k + 1, int) #there's a chance it should be float
+    for i in range(0, k + 1):
+        bounds[i] = (Q[i + 1] - Q[i]) / 2
+
+    
+
     return 0
+
+def calcError(k, *centroids, **probs):
+    for i in range(0,len(centroids)):
+        for j in range(0, len(centroids) + 1):
+
+
 
 
 # Task 3: Image histograms
