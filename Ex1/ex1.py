a3bed440e3035ea6dfe4bca4e97d1be0222fb5cb
Adar <adarlavi@gmail.com>
2017-11-16 17:13:43 +0200

    only missing the updating part, I think...

--- Ex1/ex1.py
+++ Ex1/ex1.py
@@ -11,14 +11,22 @@ def getSampledImageAtResolution(dim, pixelSize, x):
 
 # Task 2: Quantization
 def optimalQuantizationImage(img, k):
+    image = cv2.imread(img)
+    pixelsNum = image.size / 3 #maybe because its gray there's no need to divide
+
+    epsilon = 0.003
+
     Q = np.random.randint(0, 256, k)
     Q = np.sort(Q)
-    epsilon = 0.003
+
     bounds = np.zeros(k + 1, int) #there's a chance it should be float
     for i in range(0, k + 1):
         bounds[i] = (Q[i + 1] - Q[i]) / 2
 
-    while (calcError(k, Q,  ))
+    probs = calcProbs(pixelsNum, countApperances(img))
+    while (calcError(k, Q, probs) < epsilon):
+
+
 
     return 0
 
@@ -32,8 +40,14 @@ def countApperances(img):
 
     return appearances
 
-def calcError(k, centroids, appearances):
-    for i in range(0,len(centroids)):
+def calcProbs(pixelsNum,appearances):
+    probs = np.zeros(256, float)
+    for i in range(0, appearances):
+        probs[i] = appearances[i] / pixelsNum
+
+    return probs
+
+def calcError(k, centroids, probs):
 
     return 0
 
