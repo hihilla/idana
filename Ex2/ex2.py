7bba179d1885468adaf89987555a14d41a0f921a
Hilla Barkal <hilla.guz@gmail.com>
2017-11-24 15:35:45 +0200

    comments and docs

--- Ex2/ex2.py
+++ Ex2/ex2.py
@@ -1,14 +1,35 @@
 # Task 1: Geometrical transformations
 def getAffineTransformation(pts1,pts2):
+    """
+    :param: pts1,pts2 - at least 3 pairs of matched points between images A and B
+    :return: an affine transformation from image A to image B
+    """
+    # built-in numpy method for finding the least-squares solution for
+    # linear systems using Singular Value Decomposition (SVD)
+    # https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.linalg.lstsq.html
     return 0
 
 def applyAffineTransToImage(img, affineT):
+    """
+    :param: an image A and an affine transformation T
+    :return: the transformed image T*A
+    """
+    # should implement a bi-linear interpolation function to calculate new pixel values
     return 0
 
 def multipleSegmentDefromation(img, Qs, Ps, Qt, Pt, p, b):
+    """
+    :param: an image and two sets of corresponding lines (draw on two different images).
+    Each line is represented by two points (start and end).
+    And the two algorithm parameters (p, b)
+    :return: the deformed calculated from these lines using the Multiple Segment
+    Warping algorithm
+    """
+    # should use the bilinear interpolation you implemented in 1.b.
     return 0
 
 # Task 2: Image Gradients
 
 def imGradSobel(img):
+
     return 0
