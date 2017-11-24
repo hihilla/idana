87b5d8190ffe6ceb8a5c7c049e51b3c5a83a44ba
Hilla Barkal <hilla.guz@gmail.com>
2017-11-24 15:13:43 +0200

    ex2 skeleton

---
+++ Ex2/ex2.py
@@ -0,0 +1,12 @@
+# Task 1: Geometrical transformations
+
+def applyAffineTransToImage(img, affineT):
+    return 0
+
+def multipleSegmentDefromation(img, Qs, Ps, Qt, Pt, p, b):
+    return 0
+
+# Task 2: Image Gradients
+
+def imGradSobel(img):
+    return 0
