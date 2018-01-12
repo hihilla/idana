fafc5cfc986f96dbb4c66964dbe76fdf30c0b18a
Hilla Barkal <hilla.guz@gmail.com>
2018-01-12 16:22:43 +0200

    Task 1

--- Ex5/ex5.py
+++ Ex5/ex5.py
@@ -1,16 +1,26 @@
 import numpy as np
-import scipy.signal.convolve2d as conv
+from scipy.signal import convolve2d
 
 # Task 1: Gaussian pyramid
 def gaussianPyramid(img, numOfLevels, filterParam):
-    return 0
+    G = {0: img}
+    for i in np.arange(1, numOfLevels):
+        G[i] = reduce(G[i - 1], filterParam)
+    return G
 
 def generate5x5Kernel(filterParam):
+    # using gaussian weights
     kernelVector = [0.25 - filterParam / 2.0, 0.25, filterParam, 0.25, 0.25 - filterParam / 2.0]
     kernelVector = np.array(kernelVector)
-    np.outer(kernelVector, kernelVector)
+    return np.outer(kernelVector, kernelVector)
 
-def reduce
+def reduce(img, filterParam):
+    # reduce image by convolution with gaussian filter
+    kernel = generate5x5Kernel(filterParam)
+    # convolution between image and kernel to reduce image by 1/2 (same dim as image)
+    convoluted = convolve2d(img, kernel, "same")
+    # take second pixel
+    return convoluted[::2, ::2]
 
 # bonus
 def imConv2(img, kernel1D):
